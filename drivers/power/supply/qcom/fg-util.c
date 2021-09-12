f912e3f55fa46019fae33ecb00397be7b1dff728
pranavkapoor001 <pranavkapoor001@gmail.com>
2021-09-12 21:31:50 +0700

    power: Revert recent bcl reset patches
    * This randomly stops charging, revert until its fixed
    
    * Revert:
    - iio: qcom-rradc: Update logic to monitor health of RRADC peripheral
    - qcom: fg-memif: Correct timeout condition for memory grant
    - power: qpnp-fg-gen3: Add a property to reset FG BCL device
    - power_supply: add FG_RESET_CLOCK property
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: Daffa <kumaraprstya@gmail.com>

--- drivers/power/supply/qcom/fg-util.c
+++ drivers/power/supply/qcom/fg-util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2017, 2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -421,7 +421,7 @@ int fg_write(struct fg_chip *chip, int addr, u8 *val, int len)
 
 	mutex_lock(&chip->bus_lock);
 
-	sec_access = (addr & 0x00FF) >= 0xBA;
+	sec_access = (addr & 0x00FF) > 0xD0;
 
 	if (sec_access) {
 		rc = regmap_write(chip->regmap, (addr & 0xFF00) | 0xD0, 0xA5);
@@ -463,7 +463,7 @@ int fg_masked_write(struct fg_chip *chip, int addr, u8 mask, u8 val)
 
 	mutex_lock(&chip->bus_lock);
 
-	sec_access = (addr & 0x00FF) >= 0xBA;
+	sec_access = (addr & 0x00FF) > 0xD0;
 
 	if (sec_access) {
 		rc = regmap_write(chip->regmap, (addr & 0xFF00) | 0xD0, 0xA5);
