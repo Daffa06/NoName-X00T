ab9b3b2823d2f6a9714f6ebf640260f2dd616bba
Daffa06 <kumaraprastya@gmail.com>
2022-08-27 21:30:48 +0700

    android: ion: error: redefinition of 'buffer' struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
    Signed-off-by: Daffa06 <kumaraprastya@gmail.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1400,9 +1400,8 @@ static void ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf, size_t start,
 				       size_t len,
 				       enum dma_data_direction direction)
 {
-	struct ion_buffer *buffer = dmabuf->priv;
-
 	struct ion_buffer *buffer = container_of(dmabuf->priv, typeof(*buffer), iommu_data);
+	
 	mutex_lock(&buffer->lock);
 	ion_buffer_kmap_put(buffer);
 	mutex_unlock(&buffer->lock);
