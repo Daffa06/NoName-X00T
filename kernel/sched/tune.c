8bf01587ed0b8c4b0ec1ca859c34a763639f35a5
Daffa06 <kumaraprastya@gmail.com>
2022-09-05 02:42:03 +0700

    add schedtune sched_boost support
    Signed-off-by: Daffa06 <kumaraprastya@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -797,6 +797,7 @@ struct st_data {
 	char *name;
 	int boost;
 	bool prefer_idle;
+	int sched_boost;
 };
 
 static void write_default_values(struct cgroup_subsys_state *css)
@@ -811,11 +812,12 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		struct st_data tgt = st_targets[i];
 
 		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
-			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d sched_boost=%d\n",
 				tgt.name, tgt.boost, tgt.prefer_idle);
 
 			boost_write(css, NULL, tgt.boost);
 			prefer_idle_write(css, NULL, tgt.prefer_idle);
+			sched_boost_write(css, NULL, tgt.sched_boost);
 		}
 	}
 }
